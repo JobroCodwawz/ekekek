0c7799b1f370e16ea92b611fae87c3a3416156b8
Dani <holmesdani1@myvuw.ac.nz>
2023-12-10 22:39:23 +1300

    Added different jump types

--- Boobie.gd
+++ Boobie.gd
@@ -2,34 +2,42 @@ extends CharacterBody2D
 
 
 const SPEED = 150.0
-const JUMP_VELOCITY = -250.0
+const JUMP_VELOCITY = -275.0
 var target_pos:int = 0
 const TILE_SIZE:int = 32
 var last_direction:int = 1
 var snapping:bool = false
 const MOVE_DELAY:float = 0.25
 var running_delay:float = 0
+var jumping_up:bool = false
+signal clearance_height
+var y
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+func _ready():
+	clearance_height.connect(Callable(self, "jump_up"))
+	y = global_position.y
 
 func _physics_process(delta):
+	
 	# Add the gravity.
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if velocity.y < 0 and y - global_position.y >= TILE_SIZE:
+			emit_signal("clearance_height")
 
-	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor() and velocity.x == 0:
 		var dir = get_dir()
 		if !dir:
-			velocity.y = JUMP_VELOCITY
+			y = global_position.y
+			jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	if is_on_floor():
-		horizontal_move(delta)
+	horizontal_move(delta)
 
 	move_and_slide()
 	
@@ -38,19 +46,23 @@ func horizontal_move(delta):
 	running_delay += delta
 	
 	var direction = get_dir()
-	if direction and velocity.x == 0:
+	if direction and velocity.x == 0 and is_on_floor():
 		if direction != last_direction:
 			last_direction = direction
 			$Sprite2D.flip_h = !$Sprite2D.flip_h
+			$Colliders.flip()
 			start_delay()
-		elif delay_finished():
+		elif delay_finished() and not $Colliders.front_block:
 			velocity.x = direction * SPEED
 		
 	else:
-		if velocity.x != 0:
+		snap_movement()
+		
+
+func snap_movement():
+	if velocity.x != 0:
 			if not snapping:
-				if last_direction == 1: target_pos = int(ceil(global_position.x / TILE_SIZE))*TILE_SIZE
-				else: target_pos = int(floor(global_position.x / TILE_SIZE))*TILE_SIZE
+				get_target(global_position.x)
 				snapping = true
 			elif snapping and abs(global_position.x - target_pos) < 4:
 				global_position.x = target_pos
@@ -67,3 +79,32 @@ func delay_finished():
 func get_dir():
 	var direction = Input.get_axis("ui_left", "ui_right")
 	return direction
+
+func jump():
+	print($Colliders.normal_jump(), ", ", $Colliders.jump_up())
+	
+	if $Colliders.normal_jump():
+		velocity.y = JUMP_VELOCITY
+	elif $Colliders.jump_up():
+		jumping_up = true
+		velocity.y = JUMP_VELOCITY
+	elif $Colliders.jump_over():
+		velocity.y = JUMP_VELOCITY
+		velocity.x = last_direction * SPEED
+		global_position.x += last_direction
+		snapping = true
+		get_target(global_position.x + last_direction*TILE_SIZE)
+	
+func jump_up():
+	if jumping_up:
+		velocity.x = last_direction * SPEED
+		global_position.x += last_direction
+		jumping_up = false
+		snapping = true
+		get_target(global_position.x)
+	
+func get_target(pos):
+	if last_direction == 1: target_pos = int(ceil(pos / TILE_SIZE))*TILE_SIZE
+	else: target_pos = int(floor(pos / TILE_SIZE))*TILE_SIZE
+	
+	
---
+++ PlayerColliders.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+var front_block:bool = false
+var above_block:bool = false
+var below_block:bool = false
+
+func _ready():
+	front_block = $FrontCollider.get_overlapping_bodies().size() != 0
+	above_block = $AboveCollider.get_overlapping_bodies().size() != 0
+	below_block = $BelowCollider.get_overlapping_bodies().size() != 0
+	
+func normal_jump():
+	return (below_block and not(front_block or above_block)) or (front_block and above_block)
+	
+func jump_up():
+	return front_block and !above_block
+	
+func jump_over():
+	return !(front_block or above_block or below_block)
+
+
+func flip():
+	for child in get_children():
+		child.scale.x = -child.scale.x
+
+func _on_front_collider_body_entered(body):
+	front_block = true
+
+func _on_above_collider_body_entered(body):
+	above_block = true
+
+func _on_below_collider_body_entered(body):
+	below_block = true
+
+func _on_front_collider_body_exited(body):
+	front_block = false
+
+func _on_above_collider_body_exited(body):
+	above_block = false
+
+func _on_below_collider_body_exited(body):
+	below_block = false
