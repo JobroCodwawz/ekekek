5a0d576e8d4d75c895b645d6105a8870eca113cb
Dani <holmesdani1@myvuw.ac.nz>
2023-12-10 19:54:17 +1300

    Added tile based movement

--- Boobie.gd
+++ Boobie.gd
@@ -1,16 +1,20 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
+var target_pos:int = 0
+const TILE_SIZE:int = 32
+var last_direction:int = 0
+var snapping:bool = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
-func _physics_process(delta):
+func _process(delta):
 	# Add the gravity.
-	print(global_position) 
+	#print(global_position) 
 	
 	
 	if not is_on_floor():
@@ -25,7 +29,19 @@ func _physics_process(delta):
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		last_direction = direction
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if velocity.x != 0:
+			if not snapping:
+				if last_direction == 1: target_pos = int(ceil(global_position.x / TILE_SIZE))*32
+				else: target_pos = int(floor(global_position.x / TILE_SIZE))*32
+				
+				print(target_pos, ", ", global_position.x)
+				#print(target_pos)
+				snapping = true
+			elif snapping and abs(global_position.x - target_pos) < 4:
+				global_position.x = target_pos
+				velocity.x = 0
+				snapping = false
 
 	move_and_slide()
