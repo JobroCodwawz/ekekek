a28b2acea6a3cbf637b47cac355aa110bc3384b2
Dani <holmesdani1@myvuw.ac.nz>
2023-12-13 05:57:33 +1300

    ladders and platforms implemented.

--- Boobie.gd
+++ Boobie.gd
@@ -6,12 +6,19 @@ const JUMP_VELOCITY = -275.0
 var target_pos:int = 0
 const TILE_SIZE:int = 32
 var last_direction:int = 1
+var last_climb_dir:int = 0
+
 var snapping:bool = false
+var climb_snapping:bool = false
+
 const MOVE_DELAY:float = 0.25
 var running_delay:float = 0
 var jumping_up:bool = false
 signal clearance_height
 var y
+var y_offset:float
+var y_pos
+var in_jump:bool = false
 
 var projectile_scene:PackedScene
 var bomb_scene:PackedScene
@@ -21,25 +28,43 @@ var time_since_shoot:float = cooldown
 var crouched:bool = false
 
 var walking:bool = false
+var climbing:bool = false
 
 var jump_queue_time = 10
 var jump_queue_countdown = 0
+
+var on_ladder = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
+	y_offset = global_position.y
 	clearance_height.connect(Callable(self, "jump_up"))
 	y = global_position.y
 	projectile_scene = preload("res://projectile.tscn")
 	bomb_scene = preload("res://bomb.tscn")
 
 func _physics_process(delta):
+	if in_jump and velocity.x == 0 and velocity.y == 0:
+		in_jump = false
+		if $Colliders.is_ladder($Colliders/HeadCollider.global_position):
+			print("from jump")
+			on_ladder = true
+
+	y_pos = global_position.y - y_offset
+	
 	jump_queue_countdown -= 1
 	
 	if velocity.x == 0 and not get_dir():
 		walking = false
 	
+	#if $Colliders.is_ladder($Colliders/HeadCollider.global_position) or climbing:
+	#	on_ladder = true
+	#else:
+	#	on_ladder = false
+	if not $Colliders.is_ladder($Colliders/HeadCollider.global_position) and not climbing:
+		on_ladder = false
 	
 	if not walking:
 		$AnimatedSprite2D.animation = "default"
@@ -50,12 +75,17 @@ func _physics_process(delta):
 		$AnimatedSprite2D.animation = "jump"
 	elif velocity.y > 0:
 		$AnimatedSprite2D.animation = "fall"
+		
+	if on_ladder:
+		$AnimatedSprite2D.animation = "climb_idle"
+		if climbing:
+			$AnimatedSprite2D.animation = "climb"
 	
 	
 	time_since_shoot += delta
 	# Add the gravity.
 	
-	if not is_on_floor():
+	if not is_on_floor() and not on_ladder:
 		velocity.y += gravity * delta
 		velocity.y = clamp(velocity.y, JUMP_VELOCITY, -JUMP_VELOCITY)
 		
@@ -77,19 +107,64 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	horizontal_move(delta)
 	
-	
+	if on_ladder:
+		climb(0)
 	
 	if Input.is_action_pressed("ui_accept") and time_since_shoot > cooldown:
 		shoot()
 		time_since_shoot = 0
 		
-	if Input.is_action_just_pressed("ui_down") and is_on_floor() and velocity.x == 0:
-		crouch()
+	if Input.is_action_just_pressed("ui_down") and is_on_floor() and velocity.x == 0 and not on_ladder:
+		if $Colliders.is_ladder($Colliders/FootCollider.get_child(0).global_position):
+			on_ladder = true
+			climb(1)
+		else:
+			crouch()
 		
 	if Input.is_action_just_pressed("bomb"):
 		place_bomb()
 
 	move_and_slide()
+	
+func climb(dir):
+	var direction = vert_dir()
+	if dir != 0:
+		direction = dir
+		
+	
+		
+	if direction and not climbing:
+		if (not $Colliders.is_ladder($Colliders/HeadCollider.get_child(0).global_position) and dir == 0) or not $Colliders.is_ladder($Colliders/FootCollider.get_child(0).global_position):
+			get_target(y_pos, last_climb_dir)
+			climb_snapping = true
+			snap_climb()
+			return
+			
+		if direction != last_climb_dir:
+			last_climb_dir = direction
+		
+		velocity.y = direction * SPEED
+		$FullCollider.disabled = true
+		climbing = true
+	else:
+		snap_climb()
+		
+func snap_climb():
+	
+	if velocity.y != 0 and climbing:
+		if not climb_snapping:
+			get_target(y_pos, last_climb_dir)
+			climb_snapping = true
+		elif climb_snapping and abs(y_pos - target_pos) < 4:
+			global_position.y = target_pos + y_offset
+			velocity.y = 0
+			climb_snapping = false
+			climbing = false
+			$FullCollider.disabled = false
+			if not $Colliders.is_ladder($Colliders/FootCollider.get_child(0).global_position):
+				print("GROUND")
+				on_ladder = false
+			print("SNAPPED ", target_pos, ", ", y_pos, ", ", global_position.y)
 	
 	
 func horizontal_move(delta):
@@ -102,7 +177,7 @@ func horizontal_move(delta):
 			$AnimatedSprite2D.flip_h = !$AnimatedSprite2D.flip_h
 			$Colliders.flip()
 			start_delay()
-		elif delay_finished() and not $Colliders.front_block:
+		elif delay_finished() and $Colliders.can_move():
 			velocity.x = direction * SPEED
 			walking = true
 			uncrouch()
@@ -114,7 +189,7 @@ func horizontal_move(delta):
 func snap_movement():
 	if velocity.x != 0:
 			if not snapping:
-				get_target(global_position.x)
+				get_target(global_position.x, last_direction)
 				snapping = true
 			elif snapping and abs(global_position.x - target_pos) < 4:
 				global_position.x = target_pos
@@ -131,12 +206,26 @@ func delay_finished():
 func get_dir():
 	var direction = Input.get_axis("ui_left", "ui_right")
 	return direction
+	
+func vert_dir():
+	var direction = Input.get_axis("ui_up", "ui_down")
+	return direction
 
 func jump():
+	
+	if on_ladder:
+		return
+	elif $Colliders.is_ladder($CrouchCollider.global_position) and not in_jump and not crouched:
+		print("NOW ON LADDER")
+		on_ladder = true
+	
 	if crouched:
 		uncrouch()
 		return
 	
+	if not on_ladder:
+		in_jump = true
+		
 	print($Colliders.normal_jump(), ", ", $Colliders.jump_up())
 	
 	if $Colliders.normal_jump():
@@ -149,7 +238,7 @@ func jump():
 		velocity.x = last_direction * SPEED
 		global_position.x += last_direction
 		snapping = true
-		get_target(global_position.x + last_direction*TILE_SIZE)
+		get_target(global_position.x + last_direction*TILE_SIZE, last_direction)
 	
 func jump_up():
 	if jumping_up:
@@ -157,10 +246,10 @@ func jump_up():
 		global_position.x += last_direction
 		jumping_up = false
 		snapping = true
-		get_target(global_position.x)
+		get_target(global_position.x, last_direction)
 	
-func get_target(pos):
-	if last_direction == 1: target_pos = int(ceil(pos / TILE_SIZE))*TILE_SIZE
+func get_target(pos, dir):
+	if dir == 1: target_pos = int(ceil(pos / TILE_SIZE))*TILE_SIZE
 	else: target_pos = int(floor(pos / TILE_SIZE))*TILE_SIZE
 	
 	
@@ -172,20 +261,33 @@ func shoot():
 	get_parent().add_child(proj)
 
 func crouch():
-	crouched = true
-	$CollisionShape2D.scale.y = 0.5
-	$AnimatedSprite2D.scale.y = 0.5
+	var was_crouched = crouched
+	
+	if not crouched:
+		crouched = true
+		toggleCollider()
+		$AnimatedSprite2D.scale.y = 0.5
+	
+	if $Colliders.platform_below() and was_crouched:
+		uncrouch()
+		global_position.y += 1
+	
+func toggleCollider():
+	$FullCollider.disabled = !$FullCollider.disabled
+	$CrouchCollider.disabled = !$CrouchCollider.disabled
+
 	
 func uncrouch():
+	if crouched:
+		toggleCollider()
+		$AnimatedSprite2D.scale.y = 1
+		
 	crouched = false
-	$CollisionShape2D.scale.y = 1
-	$AnimatedSprite2D.scale.y = 1
-	
 	
 func place_bomb():
 	if not crouched: return
 	var bomb = bomb_scene.instantiate()
-	bomb.global_position.y = global_position.y
+	bomb.global_position.y = $CrouchCollider.global_position.y
 	bomb.global_position.x = global_position.x
 	get_parent().add_child(bomb)
 	
--- PlayerColliders.gd
+++ PlayerColliders.gd
@@ -4,21 +4,59 @@ var front_block:bool = false
 var above_block:bool = false
 var below_block:bool = false
 var above_head:bool = false
+var head:bool = false
+var foot:bool = false
+var mapref:TileMap
 
 func _ready():
 	front_block = $FrontCollider.get_overlapping_bodies().size() != 0
 	above_block = $AboveCollider.get_overlapping_bodies().size() != 0
 	below_block = $BelowCollider.get_overlapping_bodies().size() != 0
+	mapref = get_node("/root/World/TileMap")
 	
 func normal_jump():
-	return (below_block and not(front_block)) or (front_block and above_block) or above_head
+	head = $HeadCollider.overlaps_body(mapref)
+	return ((below_block and not(front_block)) or (front_block and above_block) or above_head or head) and not is_ladder($AboveCollider.get_child(0).global_position)
 	
 func jump_up():
-	return front_block and !above_block and not above_head
+	return front_block and !above_block and not above_head and not head or is_ladder($AboveCollider.get_child(0).global_position)
 	
 func jump_over():
-	return !(front_block or above_block or below_block) and not above_head
+	return !(front_block or above_block or below_block) and not above_head and not head
 
+func can_move():
+	return !front_block or is_one_way($FrontCollider.get_child(0).global_position, front_block)
+	
+func is_one_way(pos, blockcheck):
+	if not blockcheck:
+		return false
+	
+	var tile = get_tile_data(pos)
+	return tile.is_collision_polygon_one_way(0,0)
+	
+func is_ladder(pos):
+	var tile = get_tile_data(pos)
+	if tile == null: return false
+	return tile.get_custom_data("Climbable")
+	
+	
+func get_tile_data(pos):
+	var grid_pos = Vector2i(floor(pos.x/16), floor(pos.y/16))
+
+	var offset = get_parent().velocity.normalized()
+	offset = Vector2i(offset)
+	
+	var tile = mapref.get_cell_tile_data(0, grid_pos+offset)
+	return tile
+	
+		
+func platform_above():
+	return head
+	
+func platform_below():
+	foot = $FootCollider.overlaps_body(mapref)
+	return is_one_way($FootCollider.get_child(0).global_position, foot)
+	
 
 func flip():
 	for child in get_children():
@@ -26,6 +64,7 @@ func flip():
 
 func _on_front_collider_body_entered(body):
 	front_block = true
+	#print(is_one_way($FrontCollider.get_child(0).global_position))
 
 func _on_above_collider_body_entered(body):
 	above_block = true
@@ -47,3 +86,15 @@ func _on_above_head_collider_body_entered(body):
 
 func _on_above_head_collider_body_exited(body):
 	above_head = false
+
+func _on_head_collider_body_entered(body):
+	if body != get_parent(): head = true
+
+func _on_head_collider_body_exited(body):
+	head = false
+
+func _on_foot_collider_body_entered(body):
+	foot = true
+
+func _on_foot_collider_body_exited(body):
+	foot = false
